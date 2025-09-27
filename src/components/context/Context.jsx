abe2a2210593d5942e38bb6e180d78c36e8e6146
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-27 09:14:52 +0530

    context.jsx complete

--- src/components/context/Context.jsx
+++ src/components/context/Context.jsx
@@ -11,14 +11,27 @@ const ContextProvider = (props) => {
     const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
+    const delayPara = (index,nextWord )=>{
 
+    }
     const onSent = async (prompt) => {
         setResultData("");
         setLoading(true);
         setShowResult(true );
         setRecentPrompt(input);
         const response=await runChat(input);
-        setResultData(response);
+        let responseArray=response.split("**")
+        let newResponseArray;
+        for(let i=0;i<responseArray.length;i++){
+            if(i===0 ||i%2!==1){
+                newArray+=responseArray[i];
+            }   
+            else{
+                newArray+="<b>"+responseArray+"</b>";
+            }
+        }
+
+        setResultData(newArray);
         setLoading(false);
         setInput("");
     };
